ba2a81b0770efd3e295b41c33045fd9e53730a95
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-09-30 12:37:35 +0200

    termino el ejer3.php

--- ejer3.php
+++ ejer3.php
@@ -6,11 +6,24 @@
 		<?php
 			for($i=1;$i<=20;$i++){
 				$array[$i]=rand(0,50);
-			};
-			sort($array,0);
-			for($z=1;$z<=20;$z++){
-				echo $array[$z]."</br>";
+				$suma+=$array[$i];
 			}
+			$media=$suma/20;
+			sort($array,0);
+			for($z=0;$z<=20;$z++){
+				if($z==0){
+					echo "<h1 style='color:blue;'>".$array[$z]."</h1></br>";
+				}
+				elseif($z==19)
+				{
+					echo "<h1 style='color:green;'>".$array[$z]."</h1></br>";
+				}
+				else{
+					echo $array[$z]."</br>";
+				}
+			}
+			echo "la suma de todos los numeros es= ".$suma."</br>";
+			echo "la media de los numeros listados es= ".$media;
 		?>
 	</body>
 </html>
