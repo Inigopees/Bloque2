c8b700196075717383f05c1667203ec6adfc0e7b
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-09-30 16:43:56 +0200

    el ejer1.php lo he corregido

--- ejer1.php
+++ ejer1.php
@@ -7,7 +7,7 @@
 			for($i=0;$i<10;$i++){
 				$numeros[$i]=$i;
 				$factorial[$i]=gmp_fact($i);
-				echo "El factorial de ".$numeros[$i]." es ".gmp_strval($factorial[$i])."</br>";
+				echo "El factorial de ".$numeros[$i]." es ".$factorial[$i]."</br>";
 			}
 		?>
 	</body>
