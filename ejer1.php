05b707beb07bcbbf092481f5c512e402092bca0f
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-09-30 13:54:56 +0200

    para terminar no esta bien

---
+++ ejer1.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>ejer1</title>
+	</head>
+	<body>
+		<?php
+			for($i=0;$i<10;$i++){
+				$numeros[$i]=$i;
+				$factorial[$i]=gmp_fact($i);
+				echo "El factorial de ".$numeros[$i]." es ".gmp_strval($factorial[$i])."</br>";
+			}
+		?>
+	</body>
+</html>
